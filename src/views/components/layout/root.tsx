d575e2c4dd21cfe235a46ef171ca26d0b2b8a9c4
DaiSaito <saitodai3110@gmail.com>
2025-03-09 05:09:54 +0900

    noindex等

--- src/views/components/layout/root.tsx
+++ src/views/components/layout/root.tsx
@@ -8,6 +8,7 @@ const RootLayout = (props: { script?: string; css?: string; children: React.Reac
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="robots" content="noindex" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
